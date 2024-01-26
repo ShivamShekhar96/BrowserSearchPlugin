80b4b1da4ce63be022db3c539e466a95c74d8adf
Shivam Shekhar Singh <kookymonk@Shivams-MacBook-Pro.local>
2024-01-27 01:55:22 +0530

    add auth, add popups

---
+++ src/oauth.ts
@@ -0,0 +1,53 @@
+document.getElementById("login")?.addEventListener("click", () => {
+  chrome.identity.getAuthToken({ interactive: true }, async (token) => {
+    chrome.identity.getProfileUserInfo(async (userInfo) => {
+      try {
+        const payload = {
+          email: userInfo.email,
+        };
+        const requestHeaders: HeadersInit | { "X-Auth-Key": string } =
+          new Headers();
+        requestHeaders.set("Content-Type", "application/json");
+        requestHeaders.set("X-Auth-Key", token || "");
+        const response = await fetch(
+          "https://browser-search-api.onrender.com/api/v1/users/login",
+          {
+            method: "POST",
+            headers: requestHeaders,
+            body: JSON.stringify(payload),
+          }
+        );
+        chrome.action.setPopup({ popup: "popup_signed_in.html" });
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  });
+});
+
+document.getElementById("logout")?.addEventListener("click", () => {
+  chrome.identity.getAuthToken({ interactive: true }, async (token) => {
+    const current_token = token || "";
+    chrome.identity.removeCachedAuthToken(
+      { token: current_token },
+      async () => {
+        try {
+          const requestHeaders: HeadersInit | { "X-Auth-Key": string } =
+            new Headers();
+          requestHeaders.set("Content-Type", "application/json");
+          requestHeaders.set("X-Auth-Key", current_token);
+          const response = await fetch(
+            "https://browser-search-api.onrender.com/api/v1/users/logout",
+            {
+              method: "POST",
+              headers: requestHeaders,
+            }
+          );
+          chrome.action.setPopup({ popup: "popup.html" });
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    );
+  });
+});
